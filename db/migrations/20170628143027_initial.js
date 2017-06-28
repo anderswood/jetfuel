86a80a17625ad6b0c412482cbad8f570e859ad76
Anders Wood <anderswood@gmail.com>
2017-06-28 14:47:24 -0600

    initialize db

---
+++ db/migrations/20170628143027_initial.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('topics', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('links', (table) => {
+      table.increments('id').primary();
+      table.string('link_title');
+      table.string('long_link');
+      table.string('short_link');
+      table.integer('click_count');
+      table.integer('topic_id').unsigned();
+      table.foreign('topic_id').references('topics.id');
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.dropTable('links'),
+    knex.dropTable('topics')
+  ]);
+
+};
