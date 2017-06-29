993e6968b040d5d76397077c885be197b8d923e0
Anders Wood <anderswood@gmail.com>
2017-06-28 18:05:03 -0600

    updated posted to ajax

--- public/javascript.js
+++ public/javascript.js
@@ -26,24 +26,28 @@ $('#content-container').on('click', '.link-add-btn', function() {
   //using es5 because es6 changes scope of 'this'.. how to use es6?
   //POST a new link to the specific topic
   //will need variable for topic id that will be used as query parameter
-  let linkTitle = $(this).siblings('.title-input').val()
-  let link = $(this).siblings('.url-input').val()
-  let topicId = $(this).parents('.card-body-initial').siblings('.topic-title').children('h3').text();
+  let linkTitle = $(this).siblings('.title-input').val();
+  let longLink = $(this).siblings('.url-input').val();
+  let topicId = $(this).attr('id');
   console.log(topicId);
+  // let topicId = $(this).parents('.card-body-initial').siblings('.topic-title').children('h3').text();
+  let bodyObj = {
+    link_title: linkTitle,
+    long_link: longLink,
+    short_link: 'shorty',
+    click_count: 0,
+    topic_id: topicId
+  };
 
-  fetch('/api/links/', {
-    method: 'POST',
-    headers: { "content-type":"application/json" },
-    body: JSON.stringify({ link: link, linkTitle: linkTitle, topicId: topicId })
-  })
-  .then(resp => {
-    if (resp.status === 201) {
-      appendLink(linkTitle, link, this)
+  $.post('/api/v1/links/', bodyObj, (res,text,resObj) => {
+    if (resObj.status === 201) {
+      appendLink(linkTitle, 'shortLink', this)
     } else if (resp.status === 422) {
       alert('invalid link entry')
     }
   })
   .catch(error => console.log(error))
+
 })
 
 $('#content-container').on('click', '.topic-title', function(e) {
@@ -52,14 +56,14 @@ $('#content-container').on('click', '.topic-title', function(e) {
   cardBody.toggleClass('card-body-hide')
 })
 
-function appendLink(newTitle, newUrl, thisLocale) {
+function appendLink(newTitle, shortLink, thisLocale) {
   let linksContainer = $(thisLocale).closest('.form-sort-container').siblings('.links-container');
 
   linksContainer.append(
     //need to revisit how we structuring the links....later
     `<div>
       <h4>${newTitle}</h4>
-      <h4>${newUrl}</h4>
+      <h4>${shortLink}</h4>
     </div>`
   )
 }
