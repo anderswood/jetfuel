e90c9b437f6cff9571268c7ed5078d28ce15dca9
Jack Bevis <jack.bevis@gmail.com>
2017-06-30 10:25:49 -0600

    production environment setup

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,14 @@ module.exports = {
     useNullAsDefault: true,
     migrations: { directory: './db/migrations' },
     seeds: { directory: './db/seeds/test'}
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: '.db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
