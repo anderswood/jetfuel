df56c7f2a3892eb5902fca08aedbc955032f4068
Jack Bevis <jack.bevis@gmail.com>
2017-07-10 10:26:49 -0600

    add manifest.json

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 });
 
 app.get('/api/v1/topics', (req, res) => {
@@ -45,26 +45,26 @@ app.post('/api/v1/topics', (req, res) => {
 
   database('topics').insert(topic, 'id')
     .then(topic => {
-      res.status(201).json({ id: topic[0] })
+      res.status(201).json({ id: topic[0] });
     })
     .catch(error => {
-      res.status(500).json({ error })
-    })
+      res.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/links', (req, res) => {
   database('links').select()
     .then(links => {
       if (links.length) {
-        res.status(200).json(links)
+        res.status(200).json(links);
       } else {
         res.status(404).json({
           error: 'No links were found'
-        })
+        });
       }
     })
     .catch(error => {
-      res.status(500).json({ error })
+      res.status(500).json({ error });
     });
 });
 
@@ -78,13 +78,13 @@ app.get('/api/v1/topics/:topic_id/links', (request, response) => {
       } else {
         response.status(404).json({
           error: `Could not find topic with id ${request.params.topic_id}`
-        })
+        });
       }
     })
     .catch( error => {
       response.status(500).json({ error });
-    })
-})
+    });
+});
 
 app.get('/jet.fuel/:short_link', (req, res) => {
   const bodyLink = req.params.short_link;
@@ -93,12 +93,12 @@ app.get('/jet.fuel/:short_link', (req, res) => {
     short_link: `jet.fuel/${bodyLink}`
   })
   .then(url => {
-    res.redirect(301, url[0].long_link)
+    res.redirect(301, url[0].long_link);
   })
   .catch(error => {
     res.status(404).json({
       error: 'URL was not found'
-    })
+    });
   });
 });
 
@@ -111,35 +111,35 @@ app.post('/api/v1/links', (req, res) => {
         error: `Expected format: { link_title: <String>, long_link: <String>,
           short_link: <String>, click_count: <Integer>, topic_id: <Integer> }.
           You are missing a ${requiredParameter} property.`
-      })
+      });
     }
   }
 
   database('links').insert(link, 'id')
     .then(link => {
-      res.status(201).json({ id: link[0] })
+      res.status(201).json({ id: link[0] });
     })
     .catch(error => {
-      res.status(500).json({ error })
-    })
+      res.status(500).json({ error });
+    });
 });
 
 app.put('/api/v1/links/clickCountIncr', (req, res) => {
-  let updatedClickCount = parseInt(req.body.clickCount) + 1
-  
+  let updatedClickCount = parseInt(req.body.clickCount) + 1;
+
   database('links').where('short_link', req.body.shortLinkText).update('click_count', updatedClickCount)
     .then( thing => {
-      res.status(201).json({ response: 'click_count successfully incremented' })
+      res.status(201).json({ response: 'click_count successfully incremented' });
     })
     .catch( error => {
       res.status(500).json({ error });
-    })
-})
+    });
+});
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
     console.log(`Server is running on port ${app.get('port')}`);
-  })
+  });
 }
 
 module.exports = app;
